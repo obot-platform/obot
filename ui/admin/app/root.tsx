22b2dd43eca47868fe2c8f02040e0fb6cbb32ab9
Darren Shepherd <darren@acorn.io>
2024-10-21 10:07:54 -0700

    chore: workspace files updates

--- ui/admin/app/root.tsx
+++ ui/admin/app/root.tsx
@@ -38,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
+                <link rel="icon shortcut icon apple-touch-icon" href="/admin/favicon.ico" />
                 <Meta />
                 <Links />
             </head>
