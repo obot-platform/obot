ffad8082be35f3b656e885a864d17a649fc04073
Ryan Hopper-Lowe <ryan@acorn.io>
2024-10-23 14:14:44 -0500

    chore: update Oauth apps header to match other page headers
    Signed-off-by: Ryan Hopper-Lowe <ryan@acorn.io>

--- ui/admin/app/components/oauth-apps/OAuthAppList.tsx
+++ ui/admin/app/components/oauth-apps/OAuthAppList.tsx
@@ -1,4 +1,4 @@
-import { TypographyH3, TypographyP } from "~/components/Typography";
+import { TypographyH2, TypographyP } from "~/components/Typography";
 import { useOAuthAppList } from "~/hooks/oauthApps/useOAuthApps";
 
 import { OAuthAppTile } from "./OAuthAppTile";
@@ -7,9 +7,11 @@ export function OAuthAppList() {
     const apps = useOAuthAppList();
 
     return (
-        <div className="space-y-10 w-3/4 mx-auto">
+        <div className="space-y-10">
             <div>
-                <TypographyH3>Supported OAuth Apps</TypographyH3>
+                <TypographyH2 className="mb-4">
+                    Supported OAuth Apps
+                </TypographyH2>
 
                 <TypographyP className="!mt-0">
                     These are the currently supported OAuth apps for Otto. These
