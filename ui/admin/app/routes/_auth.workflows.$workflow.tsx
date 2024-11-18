61bf96f4d8a6612479053640c1e2b59ea87822a7
Ryan Hopper-Lowe <ryan@acorn.io>
2024-11-18 12:50:04 -0600

    chore: rework params section in workflow form

--- ui/admin/app/routes/_auth.workflows.$workflow.tsx
+++ ui/admin/app/routes/_auth.workflows.$workflow.tsx
@@ -5,10 +5,9 @@ import {
     useNavigate,
 } from "@remix-run/react";
 import { $path } from "remix-routes";
-import { z } from "zod";
 
 import { WorkflowService } from "~/lib/service/api/workflowService";
-import { RouteService } from "~/lib/service/routeService";
+import { RouteQueryParams, RouteService } from "~/lib/service/routeService";
 import { noop } from "~/lib/utils";
 
 import { Chat } from "~/components/chat";
@@ -20,31 +19,29 @@ import {
 } from "~/components/ui/resizable";
 import { Workflow } from "~/components/workflow";
 
-export type SearchParams = z.infer<
-    (typeof RouteService.schemas)["/workflows/:workflow"]
->;
+export type SearchParams = RouteQueryParams<"workflowSchema">;
 
 export const clientLoader = async ({
     params,
     request,
 }: ClientLoaderFunctionArgs) => {
-    const { workflow: id } = RouteService.getPathParams(
+    const { pathParams, query } = RouteService.getRouteInfo(
         "/workflows/:workflow",
+        new URL(request.url),
         params
     );
 
-    if (!id) {
-        throw redirect("/threads");
+    if (!pathParams.workflow) {
+        throw redirect($path("/workflows"));
     }
 
-    const workflow = await WorkflowService.getWorkflowById(id).catch(noop);
-    if (!workflow) throw redirect("/agents");
+    const workflow = await WorkflowService.getWorkflowById(
+        pathParams.workflow
+    ).catch(noop);
 
-    const { threadId } =
-        RouteService.getQueryParams(
-            "/workflows/:workflow",
-            new URL(request.url).search
-        ) || {};
+    if (!workflow) throw redirect($path("/workflows"));
+
+    const { threadId } = query ?? {};
 
     return { workflow, threadId };
 };
