c0796cfe97ebc8e15df9f0a27398e276afab421a
Ivy <ivy.jeong@gmail.com>
2024-12-06 19:12:21 -0500

    Update table.tsx

--- ui/admin/app/components/ui/table.tsx
+++ ui/admin/app/components/ui/table.tsx
@@ -56,7 +56,7 @@ const TableRow = React.forwardRef<
     <tr
         ref={ref}
         className={cn(
-            "transition-colors hover:bg-muted/30 data-[state=selected]:bg-muted",
+            "border-b transition-colors hover:bg-muted/30 data-[state=selected]:bg-muted",
             className
         )}
         {...props}
