77b733d8c7a49a0814747d27b3c6b10cd6e527ed
Ivy <ivy.jeong@gmail.com>
2025-01-07 10:41:13 -0500

    enhance: workflow can have multiple schedules
    Update useCronjob.tsx

--- ui/admin/app/components/workflow/WorkflowSchedulePanel.tsx
+++ ui/admin/app/components/workflow/WorkflowSchedulePanel.tsx
@@ -1,37 +1,30 @@
-import { ClockIcon } from "lucide-react";
+import { ClockIcon, PlusIcon, TrashIcon } from "lucide-react";
+
+import { CronJob } from "~/lib/model/cronjobs";
 
 import { TypographyH4 } from "~/components/Typography";
+import { Button } from "~/components/ui/button";
 import { CardDescription } from "~/components/ui/card";
-import { Label } from "~/components/ui/label";
-import { Switch } from "~/components/ui/switch";
 import { ScheduleSelection } from "~/components/workflow-triggers/shared/ScheduleSelection";
 import { useCronjob } from "~/hooks/cronjob/useCronjob";
 
+const defaultSchedule = {
+    schedule: "0 * * * *", // default: on the hour
+    description: "",
+    input: "",
+};
+
 export function WorkflowSchedulePanel({ workflowId }: { workflowId: string }) {
-    const { cronJob, createCronJob, deleteCronJob, updateCronJob } =
+    const { cronJobs, createCronJob, deleteCronJob, updateCronJob } =
         useCronjob(workflowId);
 
-    const hasCronJob = !!cronJob;
-
-    const handleCheckedChange = (checked: boolean) => {
-        if (checked) {
-            createCronJob.execute({
-                workflow: workflowId,
-                schedule: "0 * * * *", // default: on the hour
-                description: "",
-                input: "",
-            });
-        } else {
-            if (cronJob) {
-                deleteCronJob.execute(cronJob.id);
-            }
-        }
-    };
-
-    const handleCronJobScheduleUpdate = (newSchedule: string) => {
-        if (!cronJob) return;
-        updateCronJob.execute(cronJob.id, {
-            ...cronJob,
+    const handleCronJobScheduleUpdate = (
+        cronJob: CronJob,
+        newSchedule: string
+    ) => {
+        const { id: cronJobId, ...rest } = cronJob;
+        updateCronJob(cronJobId, {
+            ...rest,
             schedule: newSchedule,
         });
     };
@@ -43,29 +36,50 @@ export function WorkflowSchedulePanel({ workflowId }: { workflowId: string }) {
                     <ClockIcon className="w-4 h-4" />
                     Schedule
                 </TypographyH4>
-                <div className="flex items-center space-x-2">
-                    <Label htmlFor="schedule-switch" className="hidden">
-                        Enable Schedule
-                    </Label>
-                    <Switch
-                        id="schedule-switch"
-                        checked={hasCronJob}
-                        onCheckedChange={handleCheckedChange}
-                    />
-                </div>
             </div>
 
             <CardDescription>
                 Set up a schedule to run the workflow on a regular basis.
             </CardDescription>
 
-            <div className="flex gap-4 justify-center items-center">
-                <ScheduleSelection
-                    disabled={!hasCronJob}
-                    onChange={handleCronJobScheduleUpdate}
-                    value={cronJob?.schedule ?? ""}
-                />
+            <div className="flex flex-col gap-4 justify-center items-center">
+                {cronJobs.map((cronJob) => (
+                    <div key={cronJob.id} className="flex gap-2 w-full">
+                        <ScheduleSelection
+                            onChange={(newSchedule) =>
+                                handleCronJobScheduleUpdate(
+                                    cronJob,
+                                    newSchedule
+                                )
+                            }
+                            value={cronJob?.schedule ?? ""}
+                        />
+                        <Button
+                            size="icon"
+                            variant="ghost"
+                            onClick={() => deleteCronJob(cronJob.id)}
+                        >
+                            <TrashIcon className="w-4 h-4" />
+                        </Button>
+                    </div>
+                ))}
             </div>
+
+            <Button
+                variant="ghost"
+                className="self-end"
+                startContent={<PlusIcon />}
+                type="button"
+                loading={createCronJob.isLoading}
+                onClick={() =>
+                    createCronJob.execute({
+                        ...defaultSchedule,
+                        workflow: workflowId,
+                    })
+                }
+            >
+                Add Schedule
+            </Button>
         </div>
     );
 }
