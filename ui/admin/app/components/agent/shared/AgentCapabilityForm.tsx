3c1d795f884aff1fcee515fba6633951d4e54a47
Ryan Hopper-Lowe <ryan@acorn.io>
2025-01-16 13:39:10 -0600

    feat: make entire capability entry clickable

--- ui/admin/app/components/agent/shared/AgentCapabilityForm.tsx
+++ ui/admin/app/components/agent/shared/AgentCapabilityForm.tsx
@@ -1,7 +1,6 @@
-import { ToolReference } from "~/lib/model/toolReferences";
-
 import { ToolEntry } from "~/components/agent/ToolEntry";
 import { getCapabilityToolOrder } from "~/components/agent/shared/constants";
+import { ClickableDiv } from "~/components/ui/clickable-div";
 import { Switch } from "~/components/ui/switch";
 import { useCapabilityTools } from "~/hooks/tools/useCapabilityTools";
 
@@ -16,42 +15,36 @@ export function AgentCapabilityForm({
 }: AgentCapabilityFormProps) {
 	const { data: toolReferences } = useCapabilityTools();
 
-	const addedTools = new Set(entity.tools ?? []);
+	const currentTools = new Set(entity.tools ?? []);
 
 	const capabilities = toolReferences.toSorted(
 		(a, b) => getCapabilityToolOrder(a.id) - getCapabilityToolOrder(b.id)
 	);
 
+	const handleToggle = (id: string) => {
+		const filtered = (entity.tools ?? []).filter((t) => t !== id);
+
+		if (!currentTools.has(id)) {
+			filtered.push(id);
+		}
+
+		onChange({ tools: filtered });
+	};
+
 	return (
 		<div>
 			{capabilities.map((capability) => (
-				<ToolEntry
-					withDescription
+				<ClickableDiv
 					key={capability.id}
-					tool={capability.id}
-					actions={renderActions(capability)}
-				/>
+					onClick={() => handleToggle(capability.id)}
+				>
+					<ToolEntry
+						withDescription
+						tool={capability.id}
+						actions={<Switch checked={currentTools.has(capability.id)} />}
+					/>
+				</ClickableDiv>
 			))}
 		</div>
 	);
-
-	function renderActions(capability: ToolReference) {
-		return (
-			<Switch
-				checked={addedTools.has(capability.id)}
-				onCheckedChange={(checked) => {
-					// always filter tool out to prevent duplicate entries
-					const filtered = (entity.tools ?? []).filter(
-						(t) => t !== capability.id
-					);
-
-					if (checked) {
-						filtered.push(capability.id);
-					}
-
-					onChange({ tools: filtered });
-				}}
-			/>
-		);
-	}
 }
