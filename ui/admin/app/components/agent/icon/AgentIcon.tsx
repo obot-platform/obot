1b69c68b8f88cdd826640ff12fc85a0e7de46d1a
Ivy <ivy.jeong@gmail.com>
2025-01-21 12:38:03 -0500

    enhance: add icon for agents

---
+++ ui/admin/app/components/agent/icon/AgentIcon.tsx
@@ -0,0 +1,144 @@
+import {
+	EraserIcon,
+	ImagePlusIcon,
+	LinkIcon,
+	PaintbrushIcon,
+} from "lucide-react";
+import { useState } from "react";
+
+import { AgentIcons } from "~/lib/model/agents";
+import { AppTheme } from "~/lib/service/themeService";
+import { assetUrl } from "~/lib/utils/assetUrl";
+import { cn } from "~/lib/utils/cn";
+
+import { AgentImageUrl } from "~/components/agent/icon/AgentImageUrl";
+import { useTheme } from "~/components/theme";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { Button } from "~/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuPortal,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "~/components/ui/tooltip";
+
+const iconOptions = [
+	assetUrl("assets/agent/obot_alt_1.svg"),
+	assetUrl("assets/agent/obot_alt_2.svg"),
+	assetUrl("assets/agent/obot_alt_3.svg"),
+	assetUrl("assets/agent/obot_alt_4.svg"),
+	assetUrl("assets/agent/obot_alt_5.svg"),
+	assetUrl("assets/agent/obot_alt_6.svg"),
+	assetUrl("assets/agent/obot_alt_7.svg"),
+	assetUrl("assets/agent/obot_alt_8.svg"),
+	assetUrl("assets/agent/obot_alt_9.svg"),
+	assetUrl("assets/agent/obot_alt_10.svg"),
+];
+
+type AgentIconProps = {
+	icons?: AgentIcons;
+	onChange: (icons?: AgentIcons) => void;
+};
+
+export function AgentIcon({ icons, onChange }: AgentIconProps) {
+	const { theme } = useTheme();
+	const [imageUrlDialogOpen, setImageUrlDialogOpen] = useState(false);
+
+	const { icon = "", iconDark = "" } = icons ?? {};
+	const isDarkMode = theme === AppTheme.Dark;
+	return (
+		<>
+			<DropdownMenu>
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<DropdownMenuTrigger asChild>
+							<Button variant="ghost" size="icon-xl">
+								<Avatar className="size-24">
+									<AvatarImage
+										src={iconDark && isDarkMode ? iconDark : icon}
+										className={cn({
+											"dark:invert": !iconDark && isDarkMode,
+										})}
+									/>
+									<AvatarFallback>
+										<ImagePlusIcon />
+									</AvatarFallback>
+								</Avatar>
+							</Button>
+						</DropdownMenuTrigger>
+					</TooltipTrigger>
+					<TooltipContent>Change Agent Icon</TooltipContent>
+				</Tooltip>
+				<DropdownMenuContent className="w-52" align="start">
+					<DropdownMenuSub>
+						<DropdownMenuSubTrigger className="flex items-center gap-2">
+							<PaintbrushIcon size={16} /> Select Icon
+						</DropdownMenuSubTrigger>
+						<DropdownMenuPortal>
+							<DropdownMenuSubContent>
+								{renderIconOptions()}
+							</DropdownMenuSubContent>
+						</DropdownMenuPortal>
+					</DropdownMenuSub>
+					<DropdownMenuItem
+						className="flex items-center gap-2"
+						onClick={() => setImageUrlDialogOpen(true)}
+					>
+						<LinkIcon size={16} /> Use Image URL
+					</DropdownMenuItem>
+					<DropdownMenuItem
+						className="flex items-center gap-2"
+						onClick={() => {
+							onChange(undefined);
+						}}
+					>
+						<EraserIcon size={16} /> Clear
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+			<AgentImageUrl
+				open={imageUrlDialogOpen}
+				onOpenChange={setImageUrlDialogOpen}
+				icons={icons}
+				onChange={onChange}
+			/>
+		</>
+	);
+
+	function renderIconOptions() {
+		return (
+			<div className="grid grid-cols-5 gap-2 p-2">
+				{iconOptions.map((icon) => (
+					<DropdownMenuItem
+						key={icon}
+						onClick={() => {
+							onChange({
+								icon,
+								iconDark: "",
+								collapsed: "",
+								collapsedDark: "",
+							});
+						}}
+					>
+						<img
+							src={icon}
+							alt="Agent Icon"
+							className={cn("h-8 w-8", {
+								"dark:invert": isDarkMode,
+							})}
+						/>
+					</DropdownMenuItem>
+				))}
+			</div>
+		);
+	}
+}
