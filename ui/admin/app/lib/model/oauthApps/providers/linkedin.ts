b2ab04eb7488155a1b9c729cbf142fd2f5798521
Yingbei <yingbei@acorn.io>
2025-01-27 18:09:54 +0800

    update linkedin instruction

--- ui/admin/app/lib/model/oauthApps/providers/linkedin.ts
+++ ui/admin/app/lib/model/oauthApps/providers/linkedin.ts
@@ -23,14 +23,21 @@ const steps: OAuthFormStep<z.infer<typeof schema>>[] = [
 	{
 		type: "markdown",
 		text:
-			"### Step 1: Create a new app in Zoom App Marketplace\n" +
+			"### Step 1: Create a new app in LinkedIn Developer Portal\n" +
 			"If you already have an app, you can skip to Step 2.\n\n" +
-			"- Ensure you are logged into your preferred Zoom account.\n" +
-			"- From the [Zoom App Marketplace](https://marketplace.zoom.us/), hover over the **Develop** box on the top right corner and then select **Build App**.\n" +
-			"- When asked `What kind of app are you creating?` in the pop-up window, select **General App** and click **Create**.\n" +
-			"- Now You should be redirected to the **App Basic Information** page.\n" +
-			"- In the **Select how the app is managed** section, make sure it is set to **User-managed**.\n" +
-			"- Scroll down to the **Oauth Information** section, copy the url below and paste it into the **OAuth Redirect URL** field.\n",
+			"- Ensure you are logged into your preferred LinkedIn account.\n" +
+			"- From the [LinkedIn Developer Portal](https://developer.linkedin.com), click on **Create app**.\n" +
+			"- Input required fields: *App Name* and *LinkedIn Page*, and upload an app logo, check the legal aggrement box and click on **Create App**.\n" +
+			"- Now you should be redirected to the app's settings page.\n",
+	},
+	{
+		type: "markdown",
+		text:
+			"### Step 2: Configure the app\n" +
+			"- From the [LinkedIn Developer Portal](https://developer.linkedin.com), click **My Apps** and then select the app you'd like to proceed with.\n" +
+			"(You will already be on this page if you've completed step 1)\n" +
+			"- Select the **Products** tab and request access for **Share on LinkedIn** and **Sign In with LinkedIn using OpenID Connect**.\n" +
+			"- Then select the **Auth** tab. In the **OAuth 2.0 settings** section, click on the pencil icon to set the following as redirect URL:\n",
 	},
 	{
 		type: "copy",
@@ -38,35 +45,9 @@ const steps: OAuthFormStep<z.infer<typeof schema>>[] = [
 	},
 	{
 		type: "markdown",
-		text: "- Click **Continue** at the bottom and then click **Scopes** on the left sidebar to continue.\n",
-	},
-	{
-		type: "markdown",
-		text:
-			"### Step 2: Configure the app's necessary scopes\n" +
-			"- From the [Zoom App Management Page](https://marketplace.zoom.us/user/build), click on the app you created and click **Scopes** on the left sidebar.\n" +
-			"(You will already be on this page if you've completed step 1)\n" +
-			"- Click on **Add Scopes** to add necessary scopes for the zoom tool. Ensure the following scopes are added:\n",
-	},
-	{
-		type: "sectionGroup",
-		sections: [
-			{
-				title: "Scopes: ",
-				displayStepsInline: true,
-				defaultOpen: true,
-				steps: scopes.map((scope) => ({
-					type: "copy",
-					text: scope,
-				})),
-			},
-		],
-	},
-	{
-		type: "markdown",
 		text:
 			"### Step 3: Register your App with Obot\n" +
-			"- **Client ID** and **Client Secret** can be found in the **App Credentials** box, located at the top of the left sidebar. Alternatively, you can find them in the **App Credentials** section of the **Basic Information** page.\n" +
+			"- **Client ID** and **Client Secret** can be found in the **Application credentials** box, located at the top of the **Auth** tab page.\n" +
 			"- Copy the **Client ID** and **Client Secret** and paste them into the respective fields below.\n",
 	},
 	{ type: "input", input: "clientID", label: "Client ID" },
