43f0d6e0dcfb01e82726dd2e3edac19d584a04d6
Ivy <ivy.jeong@gmail.com>
2025-02-04 15:54:39 -0500

    Update threads.ts

--- ui/admin/test/mocks/models/threads.ts
+++ ui/admin/test/mocks/models/threads.ts
@@ -1,4 +1,6 @@
 import { RunState } from "@gptscript-ai/gptscript";
+import { mockedAgent } from "test/mocks/models/agents";
+import { mockedUser } from "test/mocks/models/users";
 
 import { Thread } from "~/lib/model/threads";
 
@@ -7,9 +9,9 @@ export const mockedThreads: Thread[] = [
 		id: "t1bm2kn",
 		created: "2025-02-04T10:53:11-05:00",
 		type: "thread",
-		agentID: "a17m9ht",
+		agentID: mockedAgent.id,
 		state: RunState.Continue,
 		lastRunID: "r1g9hw7",
-		userID: "1",
+		userID: mockedUser.id,
 	},
 ];
