72009e7aa7a28459cabe497fd77f346bb876a163
Ivy <ivy.jeong@gmail.com>
2025-02-04 15:52:12 -0500

    add test

---
+++ ui/admin/test/mocks/models/threads.ts
@@ -0,0 +1,15 @@
+import { RunState } from "@gptscript-ai/gptscript";
+
+import { Thread } from "~/lib/model/threads";
+
+export const mockedThreads: Thread[] = [
+	{
+		id: "t1bm2kn",
+		created: "2025-02-04T10:53:11-05:00",
+		type: "thread",
+		agentID: "a17m9ht",
+		state: RunState.Continue,
+		lastRunID: "r1g9hw7",
+		userID: "1",
+	},
+];
