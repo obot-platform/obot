e89ae7eaacdde1c4fb72b7fb894086a3074f837b
Ivy <ivy.jeong@gmail.com>
2025-02-11 10:56:57 -0500

    style fixes

--- ui/admin/app/components/composed/SearchInput.tsx
+++ ui/admin/app/components/composed/SearchInput.tsx
@@ -15,7 +15,6 @@ export function SearchInput({
 	const debounceOnChange = useDebounce(onChange, 300);
 	return (
 		<div className="relative">
-			<SearchIcon className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-gray-400" />
 			<Input
 				type="text"
 				placeholder={placeholder}
@@ -24,7 +23,8 @@ export function SearchInput({
 					setSearchQuery(e.target.value);
 					debounceOnChange(e.target.value);
 				}}
-				className="w-64 pl-10"
+				startContent={<SearchIcon className="h-5 w-5 text-gray-400" />}
+				className="w-64"
 			/>
 		</div>
 	);
