b4a13ba4607d2d252c5463284f094add63e0e49f
Donnie Adams <donnie@acorn.io>
2025-04-01 11:20:55 -0400

    fix: ensure running with no encryption config works
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/gateway/client/runstate.go
+++ pkg/gateway/client/runstate.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"bytes"
 	"context"
 	"errors"
 
@@ -9,12 +8,10 @@ import (
 	"gorm.io/gorm"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apiserver/pkg/storage/value"
 )
 
 var (
-	encryptionSuffix = []byte(":OBOTENCRYPTED")
-	gr               = schema.GroupResource{
+	gr = schema.GroupResource{
 		Group:    "obot.obot.ai",
 		Resource: "runstates",
 	}
@@ -23,10 +20,8 @@ var (
 func (c *Client) RunState(ctx context.Context, namespace, name string) (*types.RunState, error) {
 	r := new(types.RunState)
 	if err := c.db.WithContext(ctx).Where("name = ?", name).Where("namespace = ?", namespace).First(r).Error; err == nil {
-		if transformer, ok := c.encryptionConfig.Transformers[gr]; ok {
-			if err := decryptRunState(ctx, r, transformer); err != nil {
-				return nil, err
-			}
+		if err := c.decryptRunState(ctx, r); err != nil {
+			return nil, err
 		}
 		return r, nil
 	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
@@ -38,11 +33,11 @@ func (c *Client) RunState(ctx context.Context, namespace, name string) (*types.R
 func (c *Client) CreateRunState(ctx context.Context, runState *types.RunState) error {
 	// Copy the run state to avoid modifying the original
 	r := runState
-	if transformer, ok := c.encryptionConfig.Transformers[gr]; ok {
-		if err := encryptRunState(ctx, r, transformer); err != nil {
-			return err
-		}
+
+	if err := c.encryptRunState(ctx, r); err != nil {
+		return err
 	}
+
 	return c.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
 		// Get the run state. If it exists, return an already exists error, otherwise create it.
 		// We do this because trying to catch the gorm.ErrDuplicateKey doesn't work.
@@ -56,11 +51,11 @@ func (c *Client) CreateRunState(ctx context.Context, runState *types.RunState) e
 func (c *Client) UpdateRunState(ctx context.Context, runState *types.RunState) error {
 	// Copy the run state to avoid modifying the original
 	r := runState
-	if transformer, ok := c.encryptionConfig.Transformers[gr]; ok {
-		if err := encryptRunState(ctx, r, transformer); err != nil {
-			return err
-		}
+
+	if err := c.encryptRunState(ctx, r); err != nil {
+		return err
 	}
+
 	if err := c.db.WithContext(ctx).Save(r).Error; !errors.Is(err, gorm.ErrRecordNotFound) {
 		return err
 	}
@@ -74,58 +69,55 @@ func (c *Client) DeleteRunState(ctx context.Context, namespace, name string) err
 	return nil
 }
 
-func encryptRunState(ctx context.Context, runState *types.RunState, transformer value.Transformer) error {
+func (c *Client) encryptRunState(ctx context.Context, runState *types.RunState) error {
+	if c.encryptionConfig == nil {
+		return nil
+	}
+	transformer := c.encryptionConfig.Transformers[gr]
+	if transformer == nil {
+		return nil
+	}
+
 	var (
-		b    []byte
 		err  error
 		errs []error
 	)
-	if transformer != nil {
-		if b, err = transformer.TransformToStorage(ctx, runState.Output, nil); err != nil {
-			errs = append(errs, err)
-		} else {
-			runState.Output = append(b, encryptionSuffix...)
-		}
-		if b, err = transformer.TransformToStorage(ctx, runState.CallFrame, nil); err != nil {
-			errs = append(errs, err)
-		} else {
-			runState.CallFrame = append(b, encryptionSuffix...)
-		}
-		if b, err = transformer.TransformToStorage(ctx, runState.ChatState, nil); err != nil {
-			errs = append(errs, err)
-		} else {
-			runState.ChatState = append(b, encryptionSuffix...)
-		}
+	if runState.Output, err = transformer.TransformToStorage(ctx, runState.Output, nil); err != nil {
+		errs = append(errs, err)
+	}
+	if runState.CallFrame, err = transformer.TransformToStorage(ctx, runState.CallFrame, nil); err != nil {
+		errs = append(errs, err)
+	}
+	if runState.ChatState, err = transformer.TransformToStorage(ctx, runState.ChatState, nil); err != nil {
+		errs = append(errs, err)
 	}
 	return errors.Join(errs...)
 }
 
-func decryptRunState(ctx context.Context, runState *types.RunState, transformer value.Transformer) error {
+func (c *Client) decryptRunState(ctx context.Context, runState *types.RunState) error {
+	if c.encryptionConfig == nil {
+		return nil
+	}
+	transformer := c.encryptionConfig.Transformers[gr]
+	if transformer == nil {
+		return nil
+	}
+
 	var (
-		b    []byte
-		ok   bool
 		errs []error
 		err  error
 	)
-	if transformer != nil {
-		if b, ok = bytes.CutSuffix(runState.Output, encryptionSuffix); ok {
-			runState.Output, _, err = transformer.TransformFromStorage(ctx, b, nil)
-			if err != nil {
-				errs = append(errs, err)
-			}
-		}
-		if b, ok = bytes.CutSuffix(runState.CallFrame, encryptionSuffix); ok {
-			runState.CallFrame, _, err = transformer.TransformFromStorage(ctx, b, nil)
-			if err != nil {
-				errs = append(errs, err)
-			}
-		}
-		if b, ok = bytes.CutSuffix(runState.ChatState, encryptionSuffix); ok {
-			runState.ChatState, _, err = transformer.TransformFromStorage(ctx, b, nil)
-			if err != nil {
-				errs = append(errs, err)
-			}
-		}
+	runState.Output, _, err = transformer.TransformFromStorage(ctx, runState.Output, nil)
+	if err != nil {
+		errs = append(errs, err)
+	}
+	runState.CallFrame, _, err = transformer.TransformFromStorage(ctx, runState.CallFrame, nil)
+	if err != nil {
+		errs = append(errs, err)
+	}
+	runState.ChatState, _, err = transformer.TransformFromStorage(ctx, runState.ChatState, nil)
+	if err != nil {
+		errs = append(errs, err)
 	}
 
 	return errors.Join(errs...)
