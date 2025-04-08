af466408b8e92214694a4378c74e3ead3060a8d5
Robert Bean <beanrd7@gmail.com>
2025-04-08 15:23:10 -0400

    Fix: Download button overlaps issue in file editor

--- ui/user/src/lib/components/Editors.svelte
+++ ui/user/src/lib/components/Editors.svelte
@@ -118,7 +118,7 @@
 
 		{#if downloadable}
 			<button
-				class="icon-button absolute top-5 right-5"
+				class="icon-button absolute top-5 right-5 bg-gray-100 shadow-sm"
 				onclick={() => {
 					const selected = layout.items.find((item) => item.selected);
 					if (selected) {
