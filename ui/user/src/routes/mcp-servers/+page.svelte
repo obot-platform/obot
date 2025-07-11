3598f784e286af40020488248cc08c77a63cde77
Donnie Adams <donnie@acorn.io>
2025-07-11 19:15:29 -0400

    fix: ensure that expected project has expected MCP server when chatting
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- ui/user/src/routes/mcp-servers/+page.svelte
+++ ui/user/src/routes/mcp-servers/+page.svelte
@@ -311,35 +311,35 @@
 			(project) => project.name === connectedServer.server?.manifest.name
 		);
 
-		if (match) {
-			// go ahead and open the tab with the project
-			window.open(`/o/${match.id}`, '_blank');
-			chatLoading = false;
-			return;
+        let project = match;
+		if (!match) {
+            // if no project match, create a new one w/ mcp server connected to it
+            project = await EditorService.createObot({
+                name: connectedServer.server?.manifest.name ?? ''
+            });
 		}
 
-		// if no project match, create a new one w/ mcp server connected to it
-		const project = await EditorService.createObot({
-			name: connectedServer.server?.manifest.name ?? ''
-		});
+        if (project && !(await ChatService.listProjectMCPs(project.assistantID, project.id)).find((mcp) => mcp.manifest.name === connectedServer.server?.id)) {
+            let values: Record<string, string> = {};
+            if (connectedServer.parent) {
+                values = await ChatService.revealSingleOrRemoteMcpServer(connectedServer.server.id);
+            }
+            const mcpServerInfo = {
+                ...connectedServer.server,
+                env: connectedServer.server?.manifest.env?.map((env) => ({
+                    ...env,
+                    value: values[env.key] ?? ''
+                })),
+                headers: connectedServer.server?.manifest.headers?.map((header) => ({
+                    ...header,
+                    value: values[header.key] ?? ''
+                }))
+            };
 
-		let values: Record<string, string> = {};
-		if (connectedServer.parent) {
-			values = await ChatService.revealSingleOrRemoteMcpServer(connectedServer.server.id);
-		}
-		const mcpServerInfo = {
-			...connectedServer.server,
-			env: connectedServer.server?.manifest.env?.map((env) => ({
-				...env,
-				value: values[env.key] ?? ''
-			})),
-			headers: connectedServer.server?.manifest.headers?.map((header) => ({
-				...header,
-				value: values[header.key] ?? ''
-			}))
-		};
-		await createProjectMcp(mcpServerInfo, project);
-		window.open(`/o/${project.id}`, '_blank');
+            await createProjectMcp(mcpServerInfo, project);
+        }
+
+		window.open(`/o/${project?.id}`, '_blank');
 		chatLoading = false;
 	}
 
