cbdb27336f3c66a4128c1ce3bd279b635dea3564
Donnie Adams <donnie@obot.ai>
2025-09-22 14:04:09 -0400

    enhance: give admin access to PUP servers
    Additionally, this change gives some basic access to Power User and Power User
    Plusses for server details, restart, and logs.
    
    Signed-off-by: Donnie Adams <donnie@obot.ai>

--- pkg/api/authz/resources.go
+++ pkg/api/authz/resources.go
@@ -19,8 +19,6 @@ var apiResources = []string{
 	"GET    /mcp-connect/{mcp_id}",
 	"POST   /mcp-connect/{mcp_id}",
 	"DELETE /mcp-connect/{mcp_id}",
-	"GET    /api/mcp-stats/{mcp_id}",
-	"GET    /api/mcp-audit-logs/{mcp_id}",
 	"GET    /api/assistants",
 	"GET    /api/assistants/{assistant_id}",
 	"GET    /api/assistants/{assistant_id}/projects",
@@ -235,6 +233,11 @@ var apiResources = []string{
 	"POST   /api/workspaces/{workspace_id}/entries",
 	"DELETE /api/workspaces/{workspace_id}/entries/{entry_id}",
 	"GET    /api/workspaces/{workspace_id}/entries/{entry_id}",
+	"GET    /api/workspaces/{workspace_id}/entries/{entry_id}/servers",
+	"GET    /api/workspaces/{workspace_id}/entries/{entry_id}/servers/{mcpserver_id}",
+	"GET    /api/workspaces/{workspace_id}/entries/{entry_id}/servers/{mcpserver_id}/details",
+	"GET    /api/workspaces/{workspace_id}/entries/{entry_id}/servers/{mcpserver_id}/logs",
+	"POST   /api/workspaces/{workspace_id}/entries/{entry_id}/servers/{mcpserver_id}/restart",
 	"PUT    /api/workspaces/{workspace_id}/entries/{entry_id}",
 	"POST   /api/workspaces/{workspace_id}/entries/{entry_id}/generate-tool-previews",
 	"POST   /api/workspaces/{workspace_id}/entries/{entry_id}/generate-tool-previews/oauth-url",
@@ -266,16 +269,17 @@ type Resources struct {
 }
 
 type ResourcesAuthorized struct {
-	Assistant         *v1.Agent
-	Project           *v1.Thread
-	Thread            *v1.Thread
-	ThreadShare       *v1.ThreadShare
-	Task              *v1.Workflow
-	MCPServer         *v1.MCPServer
-	MCPServerInstance *v1.MCPServerInstance
-	Run               *v1.WorkflowExecution
-	Workflow          *v1.Workflow
-	Tool              *v1.Tool
+	Assistant          *v1.Agent
+	Project            *v1.Thread
+	Thread             *v1.Thread
+	ThreadShare        *v1.ThreadShare
+	Task               *v1.Workflow
+	MCPServer          *v1.MCPServer
+	MCPServerInstance  *v1.MCPServerInstance
+	Run                *v1.WorkflowExecution
+	Workflow           *v1.Workflow
+	Tool               *v1.Tool
+	PowerUserWorkspace *v1.PowerUserWorkspace
 }
 
 func (a *Authorizer) evaluateResources(req *http.Request, vars GetVar, user user.Info) (bool, error) {
