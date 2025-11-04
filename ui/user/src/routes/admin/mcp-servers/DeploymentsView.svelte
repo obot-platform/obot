27d184e6caba0b3b755e0afd4da2ea6194637eab
Abdel <abdelhalim.ria@gmail.com>
2025-11-04 17:08:05 +0800

    fix lint issues

--- ui/user/src/routes/admin/mcp-servers/DeploymentsView.svelte
+++ ui/user/src/routes/admin/mcp-servers/DeploymentsView.svelte
@@ -264,7 +264,7 @@
 			<LoaderCircle class="size-6 animate-spin" />
 		</div>
 	{:else if serversData.length === 0}
-		<div class="w-md my-12 flex flex-col items-center gap-4 self-center text-center">
+		<div class="my-12 flex w-md flex-col items-center gap-4 self-center text-center">
 			<Server class="dark:text-surface3 size-24 text-gray-200" />
 			<h4 class="text-lg font-semibold text-gray-400 dark:text-gray-600">
 				No current deployments.
