2e93b5b2bc379682055c1213378d4b4963b9ffff
Abdel <abdelhalim.ria@gmail.com>
2025-12-09 11:40:52 -0500

    fix: support only dynamic element atm

--- ui/user/src/lib/components/ui/teleport/Teleport.svelte
+++ ui/user/src/lib/components/ui/teleport/Teleport.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
 	import { port } from './utils';
 
-	let { class: klass = '', as = 'div', component, target, children, ...restProps } = $props();
+	let { class: klass = '', as = 'div', target, children, ...restProps } = $props();
 </script>
 
-{#if target && component}
-	<component {@attach port(target)} class={klass} {...restProps}>
-		{@render children?.()}
-	</component>
-{:else if target}
-	<svelte:element this={as} {@attach port(target)} class={klass} {...restProps}>
-		{@render children?.()}
-	</svelte:element>
-{/if}
+<svelte:element this={as} {@attach port(target)} class={klass} {...restProps}>
+	{@render children?.()}
+</svelte:element>
