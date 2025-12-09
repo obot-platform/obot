11fb3ca31b4d8e96e0c57b4baca54940eb1a1841
Nick Hale <4175918+njhale@users.noreply.github.com>
2025-12-08 19:29:19 -0500

    fix: composite mcp servers
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- pkg/api/handlers/mcphelpers.go
+++ pkg/api/handlers/mcphelpers.go
@@ -152,3 +152,46 @@ func DeleteCredentialIfExists(ctx context.Context, gptClient *gptscript.GPTScrip
 	}
 	return nil
 }
+
+// EnsureCredential will ensure a given credential exists and it's env field is up to date.
+// Returns true if a credential was created or modified to match.
+func ensureCredential(ctx context.Context, gptClient *gptscript.GPTScript, cred gptscript.Credential) (bool, error) {
+	credCtx := []string{cred.Context}
+	existing, err := gptClient.RevealCredential(ctx, credCtx, cred.ToolName)
+	if err != nil {
+		if !errors.As(err, &gptscript.ErrNotFound{}) {
+			return false, fmt.Errorf("failed to find credential: %w", err)
+		}
+
+		// Create new credential
+		if err := gptClient.CreateCredential(ctx, cred); err != nil {
+			return false, fmt.Errorf("failed to create credential: %w", err)
+		}
+		return true, nil
+	}
+
+	// Existing credential, check if it needs an update
+	var modified bool
+	for key, env := range cred.Env {
+		existingEnv, ok := existing.Env[key]
+		if !ok || existingEnv != env {
+			modified = true
+			break
+		}
+	}
+	if !modified && len(cred.Env) == len(existing.Env) {
+		// No update needed
+		return false, nil
+	}
+
+	// Remove and recreate the credential to update it
+	if err := gptClient.DeleteCredential(ctx, existing.Context, cred.ToolName); err != nil && !errors.As(err, &gptscript.ErrNotFound{}) {
+		return false, fmt.Errorf("failed to remove existing credential: %w", err)
+	}
+
+	if err := gptClient.CreateCredential(ctx, cred); err != nil {
+		return false, fmt.Errorf("failed to create credential: %w", err)
+	}
+
+	return true, nil
+}
