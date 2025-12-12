2836abd844f8fe2ff32e9aa030b4748734cec54d
Ivy <ivy.jeong@gmail.com>
2025-12-12 10:32:51 -0500

    +page.server.ts redirect

--- ui/user/src/routes/usage/+page.ts
+++
@@ -1,9 +0,0 @@
-import { profile } from '$lib/stores';
-import type { PageLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
-
-export const load: PageLoad = async () => {
-	if (profile.current?.isAdmin?.()) {
-		throw redirect(302, '/admin/usage');
-	}
-};
