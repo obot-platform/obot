1d499c8424b608755ade48420369facf514691b4
Grant Linville <grant@acorn.io>
2025-12-19 16:29:34 -0500

    fix: ui: handle optional sensitive provider parameters
    Signed-off-by: Grant Linville <grant@acorn.io>

--- ui/user/src/lib/components/admin/ProviderConfigure.svelte
+++ ui/user/src/lib/components/admin/ProviderConfigure.svelte
@@ -213,12 +213,28 @@
 						{#each optionalConfigurationParameters as parameter (parameter.name)}
 							{#if parameter.name in form}
 								<li class="flex flex-col gap-1">
-									<label for={parameter.name}>{parameter.friendlyName}</label>
-									<span class="text-gray text-xs">{parameter.description}</span>
-									{#if multipValuesInputs.has(parameter.name)}
+									<label for={parameter.name} class:text-red-500={error}
+										>{parameter.friendlyName}</label
+									>
+									{#if parameter.description}
+										<span class="text-gray text-xs">{parameter.description}</span>
+									{/if}
+									{#if parameter.sensitive}
+										<SensitiveInput
+											{error}
+											name={parameter.name}
+											bind:value={form[parameter.name]}
+											disabled={readonly}
+											textarea={parameter.multiline}
+											growable={parameter.multiline}
+										/>
+									{:else if multipValuesInputs.has(parameter.name)}
 										<MultiValueInput
 											bind:value={form[parameter.name]}
 											id={parameter.name}
+											labels={parameter.name === 'OBOT_AUTH_PROVIDER_EMAIL_DOMAINS'
+												? { '*': 'All domains' }
+												: {}}
 											class="text-input-filled"
 											placeholder={`Hit "Enter" to insert`.toString()}
 											disabled={readonly}
@@ -227,6 +243,7 @@
 										<textarea
 											id={parameter.name}
 											bind:value={form[parameter.name]}
+											class:error
 											class="text-input-filled min-h-[120px] resize-y"
 											disabled={readonly}
 											rows="5"
@@ -236,6 +253,7 @@
 											type="text"
 											id={parameter.name}
 											bind:value={form[parameter.name]}
+											class:error
 											class="text-input-filled"
 											disabled={readonly}
 										/>
