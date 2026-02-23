b000b969be311f4708d1cb7be59cc34848fb65be
Ivy <ivy.jeong@gmail.com>
2026-02-23 16:05:30 -0500

    Update FileEditor.svelte

--- ui/user/src/lib/components/nanobot/FileEditor.svelte
+++ ui/user/src/lib/components/nanobot/FileEditor.svelte
@@ -324,7 +324,7 @@
 			{:else if content}
 				<RawEditor
 					value={content}
-					filename={filename}
+					{filename}
 					disablePreview
 					class="h-full grow rounded-none border-0 bg-inherit shadow-none"
 					classes={{
