009e446fa05778f5944a382d26d9f7164bf67339
Ivy <ivy.jeong@gmail.com>
2026-03-16 14:28:34 -0400

    Update +page.ts

--- ui/user/src/routes/admin/skill-access-policies/[id]/+page.ts
+++ ui/user/src/routes/admin/skill-access-policies/[id]/+page.ts
@@ -1,12 +1,12 @@
 import { handleRouteError } from '$lib/errors';
-import { AdminService } from '$lib/services';
+import { AdminService, type SkillAccessPolicy } from '$lib/services';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params, fetch, parent }) => {
 	const { id } = params;
 	const { profile } = await parent();
 
-	let skillAccessPolicy;
+	let skillAccessPolicy: SkillAccessPolicy | undefined;
 	try {
 		skillAccessPolicy = await AdminService.getSkillAccessPolicy(id, { fetch });
 	} catch (err) {
