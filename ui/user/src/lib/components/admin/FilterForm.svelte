63c6f452b53783bd78635b539068d4d229c2b1dc
Ivy <ivy.jeong@gmail.com>
2026-04-10 14:01:55 -0400

    adjust z-index

--- ui/user/src/lib/components/admin/FilterForm.svelte
+++ ui/user/src/lib/components/admin/FilterForm.svelte
@@ -432,7 +432,7 @@
 	</div>
 	{#if !readonly}
 		<div
-			class="bg-surface1 dark:bg-background dark:text-on-surface1 sticky bottom-0 left-0 flex w-full justify-end gap-2 py-4 text-gray-400"
+			class="bg-surface1 dark:bg-background dark:text-on-surface1 sticky bottom-0 left-0 flex w-full justify-end gap-2 py-4 text-gray-400 z-50"
 			out:fly={{ x: -100, duration }}
 			in:fly={{ x: -100 }}
 		>
