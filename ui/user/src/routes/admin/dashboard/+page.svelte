5ccbddf1c04337cfd144f2f4f52b59aeed5ce06e
Ivy <ivy.jeong@gmail.com>
2026-04-15 18:08:42 -0400

    Update +page.svelte

--- ui/user/src/routes/admin/dashboard/+page.svelte
+++ ui/user/src/routes/admin/dashboard/+page.svelte
@@ -4,14 +4,17 @@
 	import DonutGraph from '$lib/components/graph/DonutGraph.svelte';
 	import StackedTimeline from '$lib/components/graph/StackedTimeline.svelte';
 	import { formatNumber } from '$lib/format';
+	import { stripMarkdownToText } from '$lib/markdown';
 	import {
 		AdminService,
+		type MCPCatalogEntry,
+		type MCPCatalogServer,
 		type OrgUser,
 		type TokenUsage,
 		type TokenUsageWithCategory,
 		type TotalTokenUsage
 	} from '$lib/services';
-	import { errors } from '$lib/stores';
+	import { errors, mcpServersAndEntries } from '$lib/stores';
 	import { aggregateTimelineDataByBucket, getUserLabels } from '../token-usage/utils';
 	import { isWithinInterval, subMonths } from 'date-fns';
 	import { Activity, ChevronRight, Coins, PencilRuler, Server, Users, Wrench } from 'lucide-svelte';
@@ -43,6 +46,89 @@
 		usersData.filter(
 			(user) => user.lastActiveDay && isWithinInterval(user.lastActiveDay, { start, end })
 		).length
+	);
+
+	function compileServerAndEntries(data: typeof mcpServersAndEntries.current) {
+		const serversMap = new Map(data.servers.map((s) => [s.id, s]));
+		const entriesMap = new Map(data.entries.map((e) => [e.id, e]));
+		const instancesCount = data.userInstances.reduce<
+			Record<string, { server: MCPCatalogServer | undefined; count: number; id: string }>
+		>((acc, instance) => {
+			if (!instance.mcpServerID) return acc;
+			if (!acc[instance.mcpServerID]) {
+				const server = serversMap.get(instance.mcpServerID);
+				if (!server) return acc;
+				acc[instance.mcpServerID] = {
+					server,
+					count: 0,
+					id: server.id
+				};
+			}
+			acc[instance.mcpServerID].count++;
+			return acc;
+		}, {});
+		const catalogEntriesCount = data.userConfiguredServers.reduce<
+			Record<string, { entry: MCPCatalogEntry | undefined; count: number; id: string }>
+		>((acc, server) => {
+			if (!server.catalogEntryID) return acc;
+			if (!acc[server.catalogEntryID]) {
+				const entry = entriesMap.get(server.catalogEntryID);
+				if (!entry) return acc;
+				acc[server.catalogEntryID] = {
+					entry,
+					count: 0,
+					id: entry.id
+				};
+			}
+			acc[server.catalogEntryID].count++;
+			return acc;
+		}, {});
+		const combine = [...Object.values(instancesCount), ...Object.values(catalogEntriesCount)];
+		// sort by count descending
+		combine.sort((a, b) => b.count - a.count);
+
+		const entrieTypes = Object.values(catalogEntriesCount).reduce(
+			(acc, info) => {
+				if (!info.entry) return acc;
+				if (info.entry.manifest.runtime === 'composite') acc.composite++;
+				else if (info.entry.manifest.runtime === 'remote') acc.remote++;
+				else acc.single++;
+				return acc;
+			},
+			{
+				single: 0,
+				remote: 0,
+				composite: 0
+			}
+		);
+
+		return {
+			graphData: [
+				{
+					label: 'Multi-User',
+					value: Object.keys(instancesCount).length
+				},
+				{
+					label: 'Single-User',
+					value: entrieTypes.single
+				},
+				{
+					label: 'Remote',
+					value: entrieTypes.remote
+				},
+				{
+					label: 'Composite',
+					value: entrieTypes.composite
+				}
+			],
+			popularServers: combine.slice(0, 5),
+			totalServers: data.userConfiguredServers.length + data.userInstances.length
+		};
+	}
+
+	const serverAndEntries = $derived(mcpServersAndEntries.current);
+	const { graphData, popularServers, totalServers } = $derived(
+		compileServerAndEntries(serverAndEntries)
 	);
 
 	onMount(async () => {
@@ -334,92 +420,70 @@
 			</div>
 		</div>
 		<div class="md:col-span-4 col-span-12 flex flex-col gap-4">
-			<div class="paper gap-2 justify-center items-center">
-				<div class="text-xs text-on-surface1">Total Active Server Connections</div>
-				<div class="flex w-full gap-2 items-center justify-center">
-					<div class="text-3xl font-semibold">100</div>
-					<Server class="size-8 text-primary" />
+			{#if serverAndEntries.loading}
+				<div class="bg-surface3 h-[138.5px] animate-pulse rounded-md"></div>
+				<div class="bg-surface3 h-[380px] animate-pulse rounded-md"></div>
+				<div class="bg-surface3 h-[380px] animate-pulse rounded-md"></div>
+			{:else}
+				<div in:fade={{ duration: 150 }} class="paper gap-2 justify-center items-center">
+					<div class="text-xs text-on-surface1">Total Active Server Connections</div>
+					<div class="flex w-full gap-2 items-center justify-center">
+						<div class="text-3xl font-semibold">{totalServers}</div>
+						<Server class="size-8 text-primary" />
+					</div>
+					<a
+						href={resolve('/admin/mcp-servers?view=deployments')}
+						class="text-[11px] transition-colors self-end translate-x-2 duration-200 bg-surface3/50 hover:bg-surface3 rounded-md py-0.5 w-fit px-2 flex items-center gap-1"
+					>
+						See All <ChevronRight class="size-3" />
+					</a>
 				</div>
-				<a
-					href={resolve('/admin/mcp-servers?view=deployments')}
-					class="text-[11px] transition-colors self-end translate-x-2 duration-200 bg-surface3/50 hover:bg-surface3 rounded-md py-0.5 w-fit px-2 flex items-center gap-1"
-				>
-					See All <ChevronRight class="size-3" />
-				</a>
-			</div>
-			<div class="paper gap-1 flex grow">
-				<h4 class="flex items-center gap-2 font-semibold">Most Popular Servers</h4>
-				<ul class="pt-2 flex flex-col gap-2">
-					<li class="flex gap-2 items-center">
-						<Server class="size-8 opacity-65" />
-						<div class="flex flex-col gap-1">
-							<p class="text-sm font-medium">Server Name</p>
-							<p class="text-xs text-on-surface1">100 calls</p>
-						</div>
-					</li>
-					<li class="flex gap-2 items-center">
-						<Server class="size-8 opacity-65" />
-						<div class="flex flex-col gap-1">
-							<p class="text-sm font-medium">Server Name</p>
-							<p class="text-xs text-on-surface1">100 calls</p>
-						</div>
-					</li>
-					<li class="flex gap-2 items-center">
-						<Server class="size-8 opacity-65" />
-						<div class="flex flex-col gap-1">
-							<p class="text-sm font-medium">Server Name</p>
-							<p class="text-xs text-on-surface1">100 calls</p>
-						</div>
-					</li>
-					<li class="flex gap-2 items-center">
-						<Server class="size-8 opacity-65" />
-						<div class="flex flex-col gap-1">
-							<p class="text-sm font-medium">Server Name</p>
-							<p class="text-xs text-on-surface1">100 calls</p>
-						</div>
-					</li>
-					<li class="flex gap-2 items-center">
-						<Server class="size-8 opacity-65" />
-						<div class="flex flex-col gap-1">
-							<p class="text-sm font-medium">Server Name</p>
-							<p class="text-xs text-on-surface1">100 calls</p>
-						</div>
-					</li>
-				</ul>
-				<div class="flex grow"></div>
-				<a
-					href={resolve('/admin/mcp-servers')}
-					class="justify-end self-end text-[11px] translate-x-2 transition-colors duration-200 bg-surface3/50 hover:bg-surface3 rounded-md py-0.5 w-fit px-2 flex items-center gap-1"
-				>
-					See All <ChevronRight class="size-3" />
-				</a>
-			</div>
-			<div class="paper">
-				<h4 class="font-semibold mb-1">Deployed Servers by Type</h4>
+				<div in:fade={{ duration: 150 }} class="paper gap-1 flex grow">
+					<h4 class="flex items-center gap-2 font-semibold">Most Popular Servers</h4>
+					<ul class="pt-2 flex flex-col gap-2">
+						{#each popularServers as info (info.id)}
+							{@const icon =
+								'server' in info ? info.server?.manifest.icon : info.entry?.manifest.icon}
+							{@const displayName =
+								'server' in info
+									? (info.server?.alias ?? info.server?.manifest.name)
+									: info.entry?.manifest.name}
+							{@const description =
+								'server' in info
+									? info.server?.manifest.description
+									: info.entry?.manifest.description}
+							<li class="flex gap-2 items-center">
+								{#if icon}
+									<img src={icon} alt={info.id} class="size-9 bg-surface1 rounded-md p-1" />
+								{:else}
+									<Server class="size-9 opacity-65 bg-surface1 rounded-md p-1" />
+								{/if}
+								<div class="flex flex-col gap-1 max-w-[calc(100%-2.5rem)]">
+									<p class="text-sm font-medium">{displayName}</p>
+									{#if description}
+										<p class="text-xs truncate line-clamp-1 break-all font-light">
+											{@html stripMarkdownToText(description ?? '')}
+										</p>
+									{/if}
+									<p class="text-xs text-on-surface1 italic">Deployed {info.count} times</p>
+								</div>
+							</li>
+						{/each}
+					</ul>
+					<div class="flex grow"></div>
+					<a
+						href={resolve('/admin/mcp-servers')}
+						class="justify-end self-end text-[11px] translate-x-2 transition-colors duration-200 bg-surface3/50 hover:bg-surface3 rounded-md py-0.5 w-fit px-2 flex items-center gap-1"
+					>
+						See All <ChevronRight class="size-3" />
+					</a>
+				</div>
+				<div in:fade={{ duration: 150 }} class="paper">
+					<h4 class="font-semibold mb-1">Deployed Servers by Type</h4>
 
-				<DonutGraph
-					class="h-72"
-					donutRatio={0.65}
-					data={[
-						{
-							label: 'Remote',
-							value: 25
-						},
-						{
-							label: 'Single-User',
-							value: 37
-						},
-						{
-							label: 'Multi-User',
-							value: 8
-						},
-						{
-							label: 'Composite',
-							value: 1
-						}
-					]}
-				/>
-			</div>
+					<DonutGraph class="h-72" donutRatio={0.65} data={graphData} />
+				</div>
+			{/if}
 		</div>
 	</div>
 </Layout>
