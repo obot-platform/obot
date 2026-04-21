228b3b174d6f1e0568435a44eeede30b9069a412
Grant Linville <grant@obot.ai>
2026-04-21 13:48:27 -0400

    add another file
    Signed-off-by: Grant Linville <grant@obot.ai>

---
+++ ui/user/src/lib/components/api-keys/SkillsLabel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	interface Props {
+		skillsAccess?: boolean;
+	}
+
+	let { skillsAccess = false }: Props = $props();
+</script>
+
+<div>
+	{#if skillsAccess}
+		All Skills
+	{:else}
+		-
+	{/if}
+</div>
