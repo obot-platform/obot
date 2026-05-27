2d953fa5a2bc90c315ca523a19e391f2b7c0cced
Donnie Adams <donnie@obot.ai>
2026-05-27 13:09:43 -0400

    chore: hide the server command (#6725)
    Signed-off-by: Donnie Adams <donnie@obot.ai>

--- pkg/cli/server.go
+++ pkg/cli/server.go
@@ -10,6 +10,10 @@ type Server struct {
 	services.Config
 }
 
+func (s *Server) Customize(cmd *cobra.Command) {
+	cmd.Hidden = true
+}
+
 func (s *Server) Run(cmd *cobra.Command, _ []string) error {
 	return server.Run(cmd.Context(), s.Config)
 }
