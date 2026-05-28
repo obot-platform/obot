b85cb556ed395e94c9220754f3392a34ef4780f5
Ivy <ivy@acorn.io>
2026-05-28 17:30:22 -0400

    fix: basic users usage graph fixes (#6756)

--- ui/user/src/routes/usage/+page.svelte
+++ ui/user/src/routes/usage/+page.svelte
@@ -9,6 +9,10 @@
 
 <Layout classes={{ container: '', childrenContainer: 'max-w-none' }} title="Usage">
 	<div class="mb-4 flex h-dvh min-h-full flex-col gap-8 pb-8" in:fade={{ duration }}>
-		<UsageGraphs mcpId={null} mcpServerCatalogEntryName={null} mcpServerDisplayName={null} />
+		<UsageGraphs />
 	</div>
 </Layout>
+
+<svelte:head>
+	<title>Obot | Usage</title>
+</svelte:head>
