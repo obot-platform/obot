8e0aaf1a06ce050b9d47a3171599c1f59575a831
Ivy <ivy.jeong@gmail.com>
2026-06-16 13:19:14 -0400

    fix: obot icon in navigation layout

---
+++ ui/user/src/lib/icons/LogoIcon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { appPreferences } from '$lib/stores';
+
+	let logoSrc = $derived(appPreferences.current.logos?.logoIcon);
+</script>
+
+<div
+	class="size-5"
+	role="img"
+	aria-label="Obot logo icon"
+	style="background-color: currentColor; mask: url({logoSrc}) center / contain no-repeat; -webkit-mask: url({logoSrc}) center / contain no-repeat;"
+></div>
